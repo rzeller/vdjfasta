a0423836f342915e3c70996064bc211ccc5a7f98
rzeller <zeller@factor-14.com>
2014-12-13 08:40:25 -0800

    set file locations

--- lib/VDJFasta.pm
+++ lib/VDJFasta.pm
@@ -67,9 +67,9 @@ sub new {
   $self->{VhVkHMM}      = "$rootdir/db/Vh-linker-Vk.hmm";
   #$self->{VhVkHMM}      = "/tools/vdjfasta/db/Vh-linker-Vk.hmm";
  
-  $self->{blast}        = "/Users/Robby/Documents/Factor14/ImmuneRepertoire/ncbi-blast-2.2.25+/bin/blastn";
-  $self->{hmmsearch}    = "/Users/Robby/Documents/Factor14/ImmuneRepertoire/hmmer-3.0/src/hmmsearch";
-  $self->{hmmalign}     = "/Users/Robby/Documents/Factor14/ImmuneRepertoire/hmmer-3.0/src/hmmalign";
+  $self->{blast}        = "/home/ncbi-blast-2.2.25+/bin/blastn";
+  $self->{hmmsearch}    = "/home/hmmer-3.0/src/hmmsearch";
+  $self->{hmmalign}     = "/home/hmmer-3.0/src/hmmalign";
 
   $self->{verbose}      = 0;
 
